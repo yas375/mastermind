7dcdd77a9f6ab799b6d7ddf0bd1f5105fc84854d
yas <ilyukevich.victor@gmail.com>
2010-05-21 21:24:32 +0300

    finish refactor in second chapter

--- spec/mastermind/game_spec.rb
+++ spec/mastermind/game_spec.rb
@@ -3,17 +3,17 @@ require File.join(File.dirname(__FILE__), "/../spec_helper" )
 module Mastermind
   describe Game do
     context "starting up" do
+      before(:each) do
+        @messenger = mock("messenger").as_null_object
+        @game = Game.new(@messenger)
+      end
       it "should send a welcome message" do
-        messenger = mock("messenger" ).as_null_object
-        game = Game.new(messenger)
-        messenger.should_receive(:puts).with("Welcome to Mastermind!")
-        game.start
+        @messenger.should_receive(:puts).with("Welcome to Mastermind!")
+        @game.start
       end
       it "should prompt for the first guess" do
-        messenger = mock("messenger" ).as_null_object
-        game = Game.new(messenger)
-        messenger.should_receive(:puts).with("Enter guess:" )
-        game.start
+        @messenger.should_receive(:puts).with("Enter guess:" )
+        @game.start
       end
     end
   end
